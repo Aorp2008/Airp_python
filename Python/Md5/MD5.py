08e0f106fc2c757c272b8fa36f602a8d618eeb69
Aorp2008 <123627593+Aorp2008@users.noreply.github.com>
2023-06-27 11:49:38 +0800

    Update MD5.py

--- Python/Md5/MD5.py
+++ Python/Md5/MD5.py
@@ -1,104 +1,100 @@
 import hashlib
-import sys
-import datetime
-import time
-i = datetime.datetime.now()
-print('运行时间为是{}月{}日{}点{}分{}秒'.format(i.month, i.day, i.hour, i.minute, i.second))
-print("开始第一次自检")
-time.sleep(1)
-print("自建完毕")
-jiami = "inti"
-enter_main = "yes"
-sha_file_main = "no"
-md5_file_main = "no"
-main = "no"
-python_quit = "no"
-quit_enter = "no"
-while quit_enter == "no":
-    print("目前已支持的加密方式有[md5,sha]")
-    while main == "no":
-        jiami = (input("请选择加密方式:"))
-        if jiami in ["md5", "sha"]:
-            main = "yes"
-        else:
-            main = "no"
-            print("错误的加密方式")
 
-    if main == "yes":
-        message = (input("请输入原文:"))
-    else:
-        sys.exit(0)
-    if jiami == "md5":
-        if main == "yes":
-            md5 = hashlib.md5(message.encode())
-            print("正在加密，请稍后")
-            time.sleep(1)
-            print("加密结果为", md5.hexdigest())
 
-            while md5_file_main == "no":
-                file_xuanze = (input("是否输出到文件内:"))
-                if file_xuanze in ["yes", "no"]:
-                    if file_xuanze == "yes":
-                        md5_file_main = "yes"
-                        filename = (input("保存的文件名:"))
-                        filenames = filename + ".txt"
-                        with open(filenames, 'w') as file_object:
-                            file_object.write(md5.hexdigest())
-                            time.sleep(1)
-                            print("已保存至", filenames)
-                            print("感谢使用")
-                            time.sleep(1)
-                    else:
-                        md5_file_main = "yes"
-                        print("感谢使用")
-                else:
-                    md5_file_main = "no"
-                    print("错误的输入")
+def hash_login_sha512(passwd):
+    login_sha512 = hashlib.sha512(passwd.encode())
+    return login_sha512.hexdigest()
 
-        else:
-            pass
-    else:
-        pass
 
-    if jiami == "sha":
-        if main == "yes":
-            sha1 = hashlib.sha1(message.encode())
-            print("正在加密，请稍后")
-            time.sleep(1)
-            print("加密结果为", sha1.hexdigest())
+def hash_md5(message):
+    md5 = hashlib.md5(message.encode())
+    return md5.hexdigest()
 
-            while sha_file_main == "no":
-                file_xuanze = (input("是否输出到文件内:"))
-                if file_xuanze in ["yes", "no"]:
-                    if file_xuanze == "yes":
-                        sha_file_main = "yes"
-                        filename = (input("保存的文件名:"))
-                        filenames = filename + ".txt"
-                        with open(filenames, 'w') as file_object:
-                            file_object.write(sha1.hexdigest())
-                            time.sleep(1)
-                            print("已保存至", filenames)
-                            print("感谢使用")
-                            time.sleep(1)
-                    else:
-                        sha_file_main = "yes"
-                        print("感谢使用")
-                else:
-                    sha_file_main = "no"
-                    print("错误的输入")
 
-    else:
-        pass
-    while quit_enter == "no":
-        python_quit = input("是否退出yes of no:")
-        if python_quit in ["yes", "no"]:
-            if python_quit == "yes":
-                quit_enter = "yes"
+def hash_sha1(message):
+    sha1 = hashlib.sha1(message.encode())
+    return sha1.hexdigest()
+
+
+def hash_sha256(message):
+    sha256 = hashlib.sha256(message.encode())
+    return sha256.hexdigest()
+
+
+def hash_sha512(message):
+    sha512 = hashlib.sha512(message.encode())
+    return sha512.hexdigest()
+
+
+def hash_new(message):
+    new = hashlib.new(message.encode)
+    return new.hexdigest()
+
+
+while True:
+    User_input_User_name = input("User:")
+    User_input_User_Passwd = input("Passwd:")
+    login_hash_Passwd = hash_login_sha512(User_input_User_Passwd)
+    with open("User.txt", mode="rt", encoding='utf-8') as file:
+        for User in file:
+            User_Name, User_Passwd = User.strip().split("----")
+            if User_input_User_name == User_Name and login_hash_Passwd == User_Passwd:
+                login_main = "Yes"
+                print("登陆成功")
                 break
-
-            if python_quit == "no":
-                quit_enter = "no"
-                break
-
         else:
-            print("错误的输入")
+            login_main = "No"
+            print("登陆失败")
+    if login_main == "Yes":
+        break
+while True:
+    print(r"exit(退出),hash(加密)")
+    User_input_chenge = input("<<<")
+    if User_input_chenge in ["exit", "hash"]:
+        if User_input_chenge == "exit":
+            exit()
+        elif User_input_chenge == "hash":
+            print("目前支持{md5,sha1,sha256,sha512}")
+            while True:
+                User_input_hash = input("<<<")
+                if User_input_hash == "~exit":
+                    break
+                if User_input_hash in ["md5", "sha1", "sha256", "sha512"]:
+                    if User_input_hash == "md5":
+                        while True:
+                            User_input_message = input("原文:")
+                            if User_input_message == "~exit":
+                                print("目前支持{md5,sha1,sha256,sha512}")
+                                break
+                            else:
+                                pass
+                            print(hash_md5(User_input_message))
+                    elif User_input_hash == "sha1":
+                        while True:
+                            User_input_message = input("原文:")
+                            if User_input_message == "~exit":
+                                print("目前支持{md5,sha1,sha256,sha512}")
+                                break
+                            else:
+                                pass
+                            print(hash_sha1(User_input_message))
+                    elif User_input_hash == "sha256":
+                        while True:
+                            User_input_message = input("原文:")
+                            if User_input_message == "~exit":
+                                print("目前支持{md5,sha1,sha256,sha512}")
+                                break
+                            else:
+                                pass
+                            print(hash_sha256(User_input_message))
+                    elif User_input_hash == "sha512":
+                        while True:
+                            User_input_message = input("原文:")
+                            if User_input_message == "~exit":
+                                print("目前支持{md5,sha1,sha256,sha512}")
+                                break
+                            else:
+                                pass
+                            print(hash_sha512(User_input_message))
+    else:
+        print("错误的输入")
